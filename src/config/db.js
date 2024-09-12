c1973790d512209fe13f75415020bf813023349d
Chaturvedi Atulkumar <chaturvediatulkumar541@gmail.com>
2024-09-12 16:16:01 +0530

    Created User Routers and User Controller

--- src/config/db.js
+++ src/config/db.js
@@ -8,7 +8,7 @@ import { DB_Name } from "../constants.js";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected to Host: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
     process.exit(1);
