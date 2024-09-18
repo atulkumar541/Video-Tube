dfcb1453dbc3e7028c5fb88bfbe425855e70f3be
Chaturvedi Atulkumar <chaturvediatulkumar541@gmail.com>
2024-09-18 19:15:56 +0530

    Added Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refereshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -31,5 +38,19 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWTToken, logoutUser);
 router.route("refresh-token").post(refereshAccessToken);
+router.route("/change-password").post(verifyJWTToken, changeCurrentPassword);
+router.route("/current-user").get(verifyJWTToken, getCurrentUser);
+router.route("/update-account").patch(verifyJWTToken, updateUserDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJWTToken, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/cover-image")
+  .patch(verifyJWTToken, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:username").get(verifyJWTToken, getUserChannelProfile);
+router.route("/history").get(verifyJWTToken, getWatchHistory);
 
 export default router;
